0e40fc2c5cc8589a9ca84fe5927675eb6ff136b8
jokil123 <jo.kil.plays@gmail.com>
2024-11-06 00:21:14 +0100

    fix

--- EsefexApi/entrypoint.sh
+++ EsefexApi/entrypoint.sh
@@ -6,4 +6,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+exec env ${MODIFIED_STARTUP}
